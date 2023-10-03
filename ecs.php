578cb3112d637578efe2138935f3ecaf6063f3e5
Julián Gutiérrez <juliangut@gmail.com>
2023-10-03 18:17:21 +0200

    temporal ignore

--- ecs.php
+++ ecs.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 use Jgut\ECS\Config\ConfigSet80;
 use PHP_CodeSniffer\Standards\Generic\Sniffs\PHP\NoSilencedErrorsSniff;
+use PhpCsFixer\Fixer\Phpdoc\PhpdocAlignFixer;
 use PhpCsFixerCustomFixers\Fixer\NoNullableBooleanTypeFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
@@ -32,16 +33,25 @@ return static function (ECSConfig $ecsConfig) use ($header): void {
         __DIR__ . '/tests',
     ]);
 
+    $skips = [
+        NoSilencedErrorsSniff::class . '.Forbidden' => [
+            __DIR__ . '/src/Configuration.php',
+            __DIR__ . '/src/Route/RouteResolver.php',
+            __DIR__ . '/src/Mapping/Annotation/Router.php',
+        ],
+        NoNullableBooleanTypeFixer::class => __DIR__ . '/src/Mapping/Attribute/Route.php',
+    ];
+
+    if (\PHP_VERSION_ID < 80_100) {
+        $skips[PhpdocAlignFixer::class] = [
+            __DIR__ . '/src/Route/Route.php',
+            __DIR__ . '/src/RouteCollector.php',
+        ];
+    }
+
     (new ConfigSet80())
         ->setHeader($header)
         ->enablePhpUnitRules()
-        ->setAdditionalSkips([
-            NoSilencedErrorsSniff::class . '.Forbidden' => [
-                __DIR__ . '/src/Configuration.php',
-                __DIR__ . '/src/Route/RouteResolver.php',
-                __DIR__ . '/src/Mapping/Annotation/Router.php',
-            ],
-            NoNullableBooleanTypeFixer::class => __DIR__ . '/src/Mapping/Attribute/Route.php',
-        ])
+        ->setAdditionalSkips($skips)
         ->configure($ecsConfig);
 };
